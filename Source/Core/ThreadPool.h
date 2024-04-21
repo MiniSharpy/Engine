46fa19aa4d2c4656689268e5fd00a0b56cfe7011
MiniSharpy <46828188+MiniSharpy@users.noreply.github.com>
2024-04-21 16:28:59 +0100

    Fix missing includes

--- Source/Core/ThreadPool.h
+++ Source/Core/ThreadPool.h
@@ -1,9 +1,21 @@
 #pragma once
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <queue>
 
 namespace Engine
 {
     // Courtesy of https://stackoverflow.com/a/32593825
     class ThreadPool {
+    private:
+        bool should_terminate = false;           // Tells threads to stop looking for jobs
+        std::mutex queue_mutex;                  // Prevents data races to the job queue
+        std::condition_variable mutex_condition; // Allows threads to wait on new jobs or termination 
+        std::vector<std::thread> threads;
+        std::queue<std::function<void()>> jobs;
+
     public:
         void Start()
     	{
@@ -65,11 +77,5 @@ namespace Engine
                 job();
             }
         }
-
-        bool should_terminate = false;           // Tells threads to stop looking for jobs
-        std::mutex queue_mutex;                  // Prevents data races to the job queue
-        std::condition_variable mutex_condition; // Allows threads to wait on new jobs or termination 
-        std::vector<std::thread> threads;
-        std::queue<std::function<void()>> jobs;
     };
 }
